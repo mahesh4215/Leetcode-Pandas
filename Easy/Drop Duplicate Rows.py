2e59673774775ae0b057eb6e323d6b0056721c23
Mahesh Tiwari <54504621+mahesh4215@users.noreply.github.com>
2025-07-02 23:30:46 +0530

    Update Drop Duplicate Rows.py

--- Easy/Drop Duplicate Rows.py
+++ Easy/Drop Duplicate Rows.py
@@ -44,6 +44,7 @@ Solution:
 import pandas as pd
 
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
-    df = customers.drop_duplicates(subset=['email'], keep='first')
-    return df
+    customers = customers.drop_duplicates(subset=['email'], keep='first')
+    return customers
+    
 
